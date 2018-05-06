f68cd11fd61cb5785c5a06b9af4cf1ba3840793d
Hayden Hudson <hayden.hudson@concept2completion.com>
2018-05-06 16:16:46 -0400

    fixing code

--- trunk/release/_release.sql
+++ trunk/release/_release.sql
@@ -1,7 +1,1 @@
-
-@trunk/packages/sample_pkg.pks;
-@trunk/build/create_tables.sql;
-@trunk/packages/sample_pkg.pkb;
-@trunk/packages/sample_data_pkg.pks;
-@trunk/packages/sample_data_pkg.pkb;
-@trunk/apex/f130.sql;
+@trunk/apex/f153.sql;
